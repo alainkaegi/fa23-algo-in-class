0998dfdbfa3c430f015321858742edb573f80c5d
Alain Kägi <16820273+alainkaegi@users.noreply.github.com>
2023-10-11 19:45:22 -0700

    test: Add deletion tests

--- test/inclass/HashSetTest.java
+++ test/inclass/HashSetTest.java
@@ -28,6 +28,39 @@ class HashSetTest {
     }
 
     @Test
+    void removesKey() {
+        s.add(1);
+        s.remove(1);
+        assertFalse(s.contains(1));
+    }
+
+    @Test
+    void doesNotRemoveOtherKey() {
+        s.add(1);
+        s.add(2);
+        s.remove(2);
+        assertTrue(s.contains(1));
+    }
+
+    @Test
+    void becomesEmptyAtCorrectTime() {
+        s.add(1);
+        s.add(2);
+        s.remove(1);
+        assertFalse(s.isEmpty());
+        s.remove(2);
+        assertTrue(s.isEmpty());
+    }
+
+    @Test
+    void doesNotStoreDuplicates() {
+        s.add(1);
+        s.add(1);
+        s.remove(1);
+        assertTrue(s.isEmpty());
+    }
+
+    @Test
     void handlesManyItems() {
         Set<String> s1 = new HashSet<>();
         String[] words = {
@@ -43,6 +76,34 @@ class HashSetTest {
             for (int j = 0; j < words.length; ++j) {
                 if (j <= i) assertTrue(s1.contains(words[j]));
                 else        assertFalse(s1.contains(words[j]));
+            }
+        }
+    }
+
+    @Test
+    void handlesManyItemsWithDeletion() {
+        Set<String> s1 = new HashSet<>();
+        String[] words = {
+                "A", "a", "aa", "aal", "aalii",
+                "aam", "Aani", "aardvark", "aardwolf", "Aaron",
+                "Aaronic", "Aaronical", "Aaronite", "Aaronitic", "Aaru",
+                "Ab", "aba", "Ababdeh", "Ababua", "abac",
+                "abaca", "abacate", "abacay"
+        };
+        for (int i = 0; i < words.length; ++i) {
+            s1.add(words[i]);
+            // After each addition, check all words for membership
+            for (int j = 0; j < words.length; ++j) {
+                if (j <= i) assertTrue(s1.contains(words[j]));
+                else        assertFalse(s1.contains(words[j]));
+            }
+        }
+        for (int i = 0; i < words.length; ++i) {
+            s1.remove(words[i]);
+            // After each deletion, check all words for membership
+            for (int j = 0; j < words.length; ++j) {
+                if (j <= i) assertFalse(s1.contains(words[j]));
+                else        assertTrue(s1.contains(words[j]));
             }
         }
     }
