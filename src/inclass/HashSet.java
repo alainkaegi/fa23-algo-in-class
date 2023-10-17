48084a3b087542dee152e0a540cef0dbb4e90e28
Alain Kägi <16820273+alainkaegi@users.noreply.github.com>
2023-10-17 07:12:33 -0700

    feat: Implement remove() with tombstones

--- src/inclass/HashSet.java
+++ src/inclass/HashSet.java
@@ -3,7 +3,9 @@ package inclass;
 public class HashSet<K> implements Set<K> {
 
     private K[] keys = (K[])new Object[2];
-    private int size = 0;
+    private int size = 0;       // excludes tombstones
+    private int actualSize = 0; // includes tombstones
+    private Object tombstone = new Object();
 
     private int hash(K key, int sz) {
         return (key.hashCode() & 0x7fffffff)%sz;
@@ -12,9 +14,10 @@ public class HashSet<K> implements Set<K> {
     private void insert(int home, K key, K[] keys) {
         assert size < keys.length/2;
         int pos = home;
-        while (keys[pos] != null) {
+        while (keys[pos] != null && keys[pos] != tombstone) {
             pos = (pos + 1) % keys.length;
         }
+        assert keys[pos] == null || keys[pos] == tombstone;
         keys[pos] = key;
     }
 
@@ -30,18 +33,30 @@ public class HashSet<K> implements Set<K> {
     private void resize(int newSize) {
         K[] data = (K[])new Object[newSize];
         for (int i = 0; i < keys.length; ++i) {
-            if (keys[i] != null) insert(hash(keys[i], data.length), keys[i], data);
+            if (keys[i] != null && keys[i] != tombstone) insert(hash(keys[i], data.length), keys[i], data);
         }
         keys = data;
+        actualSize = size;
+    }
+
+    private void clean() {
+        K[] data = (K[])new Object[keys.length];
+        for (int i = 0; i < keys.length; ++i) {
+            if (keys[i] != null && keys[i] != tombstone) insert(hash(keys[i], data.length), keys[i], data);
+        }
+        keys = data;
+        actualSize = size;
     }
 
     @Override
     public void add(K key) {
+        if (actualSize == keys.length) clean();
         if (size >= keys.length/2) resize(keys.length*2);
         int home = hash(key, keys.length);
         if (find(home, key) != -1) return;
         insert(home, key, keys);
         ++size;
+        ++actualSize;
     }
 
     @Override
@@ -57,6 +72,11 @@ public class HashSet<K> implements Set<K> {
 
     @Override
     public void remove(K key) {
-        // not implemented yet
+        int home = hash(key, keys.length);
+        int pos = find(home, key);
+        if (pos != -1) {
+            keys[pos] = (K)tombstone;
+            --size;
+        }
     }
 }
